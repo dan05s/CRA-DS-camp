80cac241e1829f91951b798954fe2093904c0d5f
dan05s <dan05s@naver.com>
2025-01-13 21:02:05 +0900

    arrayCirculateQueue completed

--- arrayCirculateQueue.c
+++ arrayCirculateQueue.c
@@ -33,20 +33,32 @@ int isFull(Queue* queue){
 }
 
 void enqueue(Queue* queue, int item){
+    if(isFull(queue)){
+        printf("Queue overflow!\n");
+    }else{
     queue -> rear = (queue -> rear + 1) % MAX_SIZE;
     queue -> data[queue -> rear] = item;
     queue -> size ++;
+    }
 }
 
 int dequeue(Queue* queue){
-
+    if(isEmpty(queue)){
+        printf("Queue underflow!\n");
+        return -1;
+    }else{
     int item = queue -> data[queue -> front];
     queue -> front = (queue -> front +1) & MAX_SIZE;
     queue -> size --;
     return item;
+    }   
 }
 
 int peak(Queue* queue){
+    if(isEmpty(queue)){
+        printf("Queue underflow!\n");
+        return -1;
+    }
     return queue -> data[queue -> front];
 }
 
@@ -57,7 +69,21 @@ int main(){
     initQueue(queue);
     printf("initializing finished!\n");
 
+    enqueue(queue,10);
+    enqueue(queue,20);
+    enqueue(queue,30);
+    dequeue(queue);
+    enqueue(queue,40);
+    dequeue(queue);
+    enqueue(queue,50);
+    enqueue(queue,60);
+    printf("Queue size: %d\n",getSize(queue));
+    
+    while(!isEmpty(queue)){
+        printf("Dequeued %d\n",dequeue(queue));
+    }
 
+    freeQueue(queue);
 
     return 0;
 }
