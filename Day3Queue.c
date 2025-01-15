035628d600947ee0a3f70b690da60f1b3789b854
dan05s <dan05s@naver.com>
2025-01-15 11:42:06 +0900

    add Day3Queue.c and edit Day3Stack.c

---
+++ Day3Queue.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = 0, rear =-1;
+
+void push(){
+
+}
+
+void pop(){
+    
+}
+
+int main(){
+    return 0;
+}
--- Day3Stack.c
+++ Day3Stack.c
@@ -1,62 +1,64 @@
-#include <stdio.h>
+#include<stdio.h>
 
 #define MAX 50
 
-char stack[MAX];
-int top = -1;
+char stack[MAX]; //데이터를 저장할 공간
+int top = -1;   //가장 마지막에 들어온 원소의 인덱스
 
-int empty() {
+//부가 기능
+int empty(){
     return top == -1;
 }
-
-int full() {
+int full(){
     return top == MAX - 1;
 }
 
-void push(char value) {
-    if (full()) {
-        printf("Stack is full\n");
-        return;
+//스택의 메인기능
+void push(char value){
+    //스택이 가득 차있는지 확인
+    if(full()){
+        printf("stack is full! \n");
+        return ;
     }
     top++;
     stack[top] = value;
 }
-
-void pop() {
-    if (empty()) {
-        printf("Stack is empty!\n");
-        return;
+void pop(){
+    //스택이 비어있는지 확인
+    if(empty()){
+        printf("stack is empty! \n");
+        return ;
     }
     top--;
 }
 
-int main() {
+int main(){
     int t;
     scanf("%d", &t);
-    for (int i = 0; i < t; i++) {
-        int isWrong =0;
+    for(int i = 0; i < t; i++){
+        int isWrong = 0;
         char arr[50];
         scanf("%s", arr);
-       for(int j=0;j<50;j++){
-        if(arr[j]=='('){
-            push(arr[j]);
-        }
-        if(arr[j]==')'){
-            if(empty()){
-                isWrong =1;
-                break;
-            }else{
-                pop();
+        for(int j = 0; j < 50; j++){
+            if(arr[j] == '('){
+                push(arr[j]);
+            }
+            if(arr[j] == ')'){
+                if(empty()){
+                    isWrong = 1;
+                    break;
+                }
+                else{
+                    pop();
+                }
             }
         }
-       }
-        if(isWrong || empty()==0){
+        if(isWrong || empty() == 0){
             printf("NO\n");
             top = -1;
-        }else{
+        }
+        else{
             printf("YES\n");
         }
     }
-
-    return 0;
 }
